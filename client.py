16d72d5c3c16794daf916caaea408b67ec4e1409
giuliano-villca <135669827+giuliano-villca@users.noreply.github.com>
2025-07-18 21:50:52 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((ip, 12345))
+
+
+mensaje = client_socket.recv(1024).decode()
+print(mensaje, end="")
+
+
+usuario = input()
+client_socket.send(usuario.encode())
+
+while True:
+    mensaje = client_socket.recv(1024).decode()
+    print(mensaje, end="")
+    comando = input()
+    client_socket.send(comando.encode())
+    respuesta = client_socket.recv(8000).decode()
+    print(respuesta)
+    if comando.strip() == "/adios":
+        break
+
+client_socket.close()
